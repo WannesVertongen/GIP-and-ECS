0e8573e298fcc4c8a8888af06ea1cb2cba6d8aa2
pieterverhamme <151650392+pieterverhamme@users.noreply.github.com>
2024-05-10 12:15:05 +0200

    new csv

--- Matlab/CSV.m
+++ Matlab/CSV.m
@@ -14,7 +14,7 @@ dx_robot = 0.8;  %horizontale verplaatsing robot [m]
 dx_object = dx_robot + l*sin(theta_opt); %horizontale verplaatsing bak [m]
 
 %% 1. RAMP INPUT 
-v = 0.2;
+v = 0.3;
 t_eind = dx_robot/v;
 ramp = v*[0:timestep:t_eind];
 
@@ -31,7 +31,7 @@ plot(time,velocity)
  writematrix(velocity,'ramp_velocity.csv')
 
 %% 2. BANG BANG INPUT
-v = 0.2;
+v = 0.3;
 T = dx_robot/v;
 
 tau1 = (0:timestep:T/2)/T;
@@ -41,8 +41,8 @@ tau2 = (T/2:timestep:T)/T;
 S1 = dx_robot*2*(tau1).^2;
 S2 = dx_robot*(-2*tau2.^2+4*tau2-1);
 
-position(1:1+ceil(T/(2*timestep))) = S1;
-position(ceil(T/(2*timestep)):ceil(T/(timestep))) = S2;        
+position(1:ceil(T/(2*timestep))) = S1;
+position(ceil(T/(2*timestep)):ceil(T/(timestep))-1) = S2;        
 position(ceil(T/timestep):end) = dx_robot;
 
 
@@ -51,8 +51,8 @@ position(ceil(T/timestep):end) = dx_robot;
 V1 = v*4*tau1;
 V2 = v*(-4*tau2 + 4);
 
-velocity(1:1+ceil(T/(2*timestep))) = V1;
-velocity(ceil(T/(2*timestep)):ceil(T/(timestep))) = V2;        
+velocity(1:ceil(T/(2*timestep))) = V1;
+velocity(ceil(T/(2*timestep)):ceil(T/(timestep))-1) = V2;        
 velocity(ceil(T/timestep):end) = 0;
 figure
 plot(time,velocity)
@@ -62,7 +62,7 @@ plot(time,velocity)
 
 %% 3. POLYNOOM INPUT
 
-v = 0.2;
+v = 0.3;
 T = dx_robot/v;
 
 tau = (0:timestep:T)/T;
@@ -70,14 +70,14 @@ tau = (0:timestep:T)/T;
 % Position 
 S = dx_robot*(-20*tau.^7+70*tau.^6-84*tau.^5+35*tau.^4);
 
-position(1:1+ceil(T/(timestep))) = S;       
+position(1:ceil(T/(timestep))) = S;       
 position(1+ceil(T/timestep):end) = dx_robot;
 
 
 % Velocity
 V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
 
-velocity(1:1+ceil(T/(timestep))) = V;      
+velocity(1:ceil(T/(timestep))) = V;      
 velocity(1+ceil(T/timestep):end) = 0;
 
  writematrix(position,'poly_position.csv')
