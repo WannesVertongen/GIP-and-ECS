2d8393eefdc70e360b5330127f063503bc78a863
pieterverhamme <151650392+pieterverhamme@users.noreply.github.com>
2024-05-10 11:47:35 +0200

    new csv

--- Matlab/CSV.m
+++ Matlab/CSV.m
@@ -10,7 +10,7 @@ time = 0:timestep:15;
 l = 1; %cable length
 theta_opt = 60.85*pi/180; %optimale eindhoek kabel [rad]
 %theta_opt = pi/4;
-dx_robot = 0.5;  %horizontale verplaatsing robot [m]
+dx_robot = 0.8;  %horizontale verplaatsing robot [m]
 dx_object = dx_robot + l*sin(theta_opt); %horizontale verplaatsing bak [m]
 
 %% 1. RAMP INPUT 
@@ -27,8 +27,8 @@ velocity(1,1:round(t_eind/timestep)+1) = v;
 figure
 plot(time,velocity)
 
-% writematrix(position,'ramp_position.csv')
-% writematrix(velocity,'ramp_velocity.csv')
+ writematrix(position,'ramp_position.csv')
+ writematrix(velocity,'ramp_velocity.csv')
 
 %% 2. BANG BANG INPUT
 v = 0.2;
@@ -41,7 +41,7 @@ tau2 = (T/2:timestep:T)/T;
 S1 = dx_robot*2*(tau1).^2;
 S2 = dx_robot*(-2*tau2.^2+4*tau2-1);
 
-position(1:ceil(T/(2*timestep))) = S1;
+position(1:1+ceil(T/(2*timestep))) = S1;
 position(ceil(T/(2*timestep)):ceil(T/(timestep))) = S2;        
 position(ceil(T/timestep):end) = dx_robot;
 
@@ -51,14 +51,14 @@ position(ceil(T/timestep):end) = dx_robot;
 V1 = v*4*tau1;
 V2 = v*(-4*tau2 + 4);
 
-velocity(1:ceil(T/(2*timestep))) = V1;
+velocity(1:1+ceil(T/(2*timestep))) = V1;
 velocity(ceil(T/(2*timestep)):ceil(T/(timestep))) = V2;        
 velocity(ceil(T/timestep):end) = 0;
 figure
 plot(time,velocity)
 
-% writematrix(position,'bangbang_position.csv')
-% writematrix(velocity,'bangbang_velocity.csv')
+ writematrix(position,'bangbang_position.csv')
+ writematrix(velocity,'bangbang_velocity.csv')
 
 %% 3. POLYNOOM INPUT
 
@@ -80,5 +80,5 @@ V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
 velocity(1:1+ceil(T/(timestep))) = V;      
 velocity(1+ceil(T/timestep):end) = 0;
 
-% writematrix(position,'poly_position.csv')
-% writematrix(velocity,'poly_velocity.csv')
+ writematrix(position,'poly_position.csv')
+ writematrix(velocity,'poly_velocity.csv')
