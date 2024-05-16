bd11cf4a5bb5327a04c135dbbd778390fbb95007
WannesVertongen <151665105+WannesVertongen@users.noreply.github.com>
2024-05-16 14:11:15 +0200

    abrupt

--- Matlab/CSV.m
+++ Matlab/CSV.m
@@ -12,37 +12,38 @@ if v_max == 0.2
 else
     offset = 0;
 end
-offset =0 ;
 
 
 % Experiment opstelling:
 l = 1; %cable length
 theta_opt = 60.85*pi/180; %optimale eindhoek kabel [rad]
 %theta_opt = pi/4;
-dx_robot = 0.8;  %horizontale verplaatsing robot [m]
+dx_robot = 1.6;  %horizontale verplaatsing robot [m]
 dx_object = dx_robot + l*sin(theta_opt); %horizontale verplaatsing bak [m]
+position = dx_robot*ones(1,length(time));
+velocity = zeros(1,length(time));
 
 %% 1. RAMP INPUT 
-v = v_max;
-t_eind = dx_robot/v;
-ramp = v*[0:timestep:t_eind];
-
-position = dx_robot*ones(1,length(time));
-position(1,1:round(t_eind/timestep)+offset) = ramp;
-
-velocity = zeros(1,length(time));
-velocity(1,1:round(t_eind/timestep)+offset) = v;
-
-figure
-plot(time,velocity)
-title("velocity profile ramp")
-figure 
-plot(time,position)
-title("position profile ramp ")
-
-
- writematrix(position,'ramp_position.csv')
- writematrix(velocity,'ramp_velocity.csv')
+% v = v_max;
+% t_eind = dx_robot/v;
+% ramp = v*[0:timestep:t_eind];
+% 
+% position = dx_robot*ones(1,length(time));
+% position(1,1:round(t_eind/timestep)+offset) = ramp;
+% 
+% velocity = zeros(1,length(time));
+% velocity(1,1:round(t_eind/timestep)+offset) = v;
+% 
+% figure
+% plot(time,velocity)
+% title("velocity profile ramp")
+% figure 
+% plot(time,position)
+% title("position profile ramp ")
+% 
+% 
+%  writematrix(position,'ramp_position.csv')
+%  writematrix(velocity,'ramp_velocity.csv')
 
 % %% 2. BANG BANG INPUT
 % 
@@ -101,7 +102,7 @@ v = v_max/2.2;
 frequency = 50;
 timestep = 1/frequency;
 time = 0:timestep:15;
-dx_robot = 0.8;  %horizontale verplaatsing robot [m]
+  %horizontale verplaatsing robot [m]
 T = dx_robot/v;
 
 
@@ -119,11 +120,13 @@ V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
 
 velocity(1:ceil(T/(timestep))+offset) = V;      
 velocity(1+ceil(T/timestep):end) = 0;
+velocity_abript = zeros(1,ceil(T/(timestep*2))+1);
+velocity_abrupt = velocity(1:ceil(T/(timestep*2)));
 
  writematrix(position,'poly_position.csv')
- writematrix(velocity,'poly_velocity.csv')
+ writematrix(velocity_abrupt,'poly_velocity_abrupt_06.csv')
 figure
-plot(time,velocity)
+plot(time(1:ceil(T/(timestep*2))),velocity_abrupt)
 title("velocity profile poly")
 figure 
 plot(time,position)
