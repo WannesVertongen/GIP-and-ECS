ba9d94ff6eb3f3b49d61ed6d6d4e0fcf2cd5f0f6
WannesVertongen <151665105+WannesVertongen@users.noreply.github.com>
2024-05-16 11:59:28 +0200

    fixed to work for both 0.2 and 0.3

--- Matlab/CSV.m
+++ Matlab/CSV.m
@@ -6,6 +6,13 @@ frequency = 50;
 timestep = 1/frequency;
 time = 0:timestep:15;
 
+v_max = 0.2;
+if v_max == 0.2
+    offset = 1;
+else
+    offset = 0;
+end
+
 % Experiment opstelling:
 l = 1; %cable length
 theta_opt = 60.85*pi/180; %optimale eindhoek kabel [rad]
@@ -14,7 +21,7 @@ dx_robot = 0.8;  %horizontale verplaatsing robot [m]
 dx_object = dx_robot + l*sin(theta_opt); %horizontale verplaatsing bak [m]
 
 %% 1. RAMP INPUT 
-v = 0.3;
+v = v_max;
 t_eind = dx_robot/v;
 ramp = v*[0:timestep:t_eind];
 
@@ -36,7 +43,7 @@ title("position profile ramp ")
  writematrix(velocity,'ramp_velocity.csv')
 
 %% 2. BANG BANG INPUT
-v_max = 0.3;
+
 v = v_max/2;
 
 
@@ -54,12 +61,12 @@ tau2 = (T/2:timestep:T)/T;
 S1 = dx_robot*2*(tau1).^2;
 S2 = dx_robot*(-2*tau2.^2+4*tau2-1);
 
-disp(size(position(ceil(T/(2*timestep)):(ceil(T/timestep)))));
+disp(size(1:ceil(T/(2*timestep)+1)));
 
-disp(size(S2));
+disp(size(S1));
 
 
-position(1:ceil(T/(2*timestep))) = S1;   
+position(1:ceil(T/(2*timestep)+offset)) = S1;   
 position(ceil(T/(2*timestep)):(ceil(T/timestep))) = S2;
 position(ceil(T/timestep):end) = dx_robot;
 
@@ -69,7 +76,8 @@ position(ceil(T/timestep):end) = dx_robot;
 V1 = v*4*tau1;
 V2 = v*(-4*tau2 + 4);
 
-velocity(1:ceil(T/(2*timestep))) = V1;
+
+velocity(1:ceil(T/(2*timestep))+offset) = V1;
 velocity(ceil(T/(2*timestep)):ceil(T/(timestep))) = V2;        
 velocity(ceil(T/timestep):end) = 0;
 figure
@@ -84,7 +92,7 @@ title("position profile bangbang ")
  writematrix(velocity,'bangbang_velocity.csv')
 
 %% 3. POLYNOOM INPUT
-v_max =0.3; 
+ 
 v = v_max/2.2;
 
 
@@ -100,14 +108,14 @@ tau = (0:timestep:T)/T;
 % Position 
 S = dx_robot*(-20*tau.^7+70*tau.^6-84*tau.^5+35*tau.^4);
 
-position(1:ceil(T/(timestep))) = S;       
+position(1:ceil(T/(timestep))+offset) = S;       
 position(1+ceil(T/timestep):end) = dx_robot;
 
 
 % Velocity
 V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
 
-velocity(1:ceil(T/(timestep))) = V;      
+velocity(1:ceil(T/(timestep))+offset) = V;      
 velocity(1+ceil(T/timestep):end) = 0;
 
  writematrix(position,'poly_position.csv')
