6f84b28c6d78ffa82a2ca0c88c6b61712aa595ba
WannesVertongen <151665105+WannesVertongen@users.noreply.github.com>
2024-05-17 10:44:04 +0200

    1 meter cable poly velocity profile

--- Matlab/CSV.m
+++ Matlab/CSV.m
@@ -6,7 +6,7 @@ frequency = 50;
 timestep = 1/frequency;
 time = 0:timestep:15;
 
-v_max = 0.2713*2;
+v_max = 0.3*2;
 if v_max == 0.2
     offset = 1;
 else
@@ -18,7 +18,7 @@ offset = 0;
 l = 1; %cable length
 theta_opt = 60.85*pi/180; %optimale eindhoek kabel [rad]
 %theta_opt = pi/4;
-dx_robot = 1.0*2;  %horizontale verplaatsing robot [m]
+dx_robot = 0.753*2;  %horizontale verplaatsing robot [m]
 dx_object = dx_robot + l*sin(theta_opt); %horizontale verplaatsing bak [m]
 position = dx_robot*ones(1,length(time));
 velocity = zeros(1,length(time));
@@ -127,8 +127,8 @@ velocity(1+ceil(T/timestep):end) = 0;
 velocity_abrupt = zeros(1,ceil(T/(timestep)));
 velocity_abrupt(1:ceil(T/(timestep*2))) = velocity(1:ceil(T/(timestep*2)));
 
- writematrix(position_abrupt,'poly_position_abrupt_053_100.csv')
- writematrix(velocity_abrupt,'poly_velocity_abrupt_053_100.csv')
+ writematrix(position_abrupt,'poly_position_abrupt_060_075.csv')
+ writematrix(velocity_abrupt,'poly_velocity_abrupt_060_075.csv')
 
 figure(1)
 plot(time(1:ceil(T/(timestep))),velocity_abrupt)
