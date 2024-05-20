ff8981920034e70c64f5aec56ce9d450d9ec36d0
WannesVertongen <151665105+WannesVertongen@users.noreply.github.com>
2024-05-20 12:35:15 +0200

    added smooth pahs

--- Matlab/csvfiles/CSV_smooth.m
+++ Matlab/csvfiles/CSV_smooth.m
@@ -48,9 +48,10 @@ velocity = zeros(1,length(time));
 velocity(1,1:round(t_eind/timestep)+offset_1) = v;
 
 figure
+subplot(2,1,1)
 plot(time,velocity)
 title("velocity profile ramp")
-figure 
+subplot(2,1,2) 
 plot(time,position)
 title("position profile ramp ")
 
@@ -95,9 +96,10 @@ velocity(ceil(T/timestep):end) = 0;
 disp(length(time))
 disp(length(velocity))
 figure
+subplot(2,1,1)
 plot(time,velocity)
 title("velocity profile bangbang")
-figure 
+subplot(2,1,2) 
 plot(time,position)
 title("position profile bangbang ")
 
@@ -107,7 +109,7 @@ writematrix(position,'pos_15_bang.csv')
 
 %% 3. POLYNOOM INPUT
  
-v = v_max/2.2;
+v = v_max;
 
 
 frequency = 50;
@@ -121,12 +123,12 @@ tau = (0:timestep:T)/T;
 % Position 
 S = dx_robot*(-20*tau.^7+70*tau.^6-84*tau.^5+35*tau.^4);
 
-position(1:ceil(T/(timestep))+offset_1) = S;       
+position(1:ceil(T/(timestep))+offset_2) = S;       
 position(1+ceil(T/timestep):end) = dx_robot;
 
-position_abrupt = zeros(1,2*ceil(T/(timestep)));
-position_abrupt(1:ceil(T/(timestep*2))) = position(1:ceil(T/(timestep*2)));
-position_abrupt(ceil(T/(timestep*2))+1:end) = position_abrupt(ceil(T/(timestep*2)));
+%position_abrupt = zeros(1,2*ceil(T/(timestep)));
+%position_abrupt(1:ceil(T/(timestep*2))) = position(1:ceil(T/(timestep*2)));
+%position_abrupt(ceil(T/(timestep*2))+1:end) = position_abrupt(ceil(T/(timestep*2)));
 
 % Velocity
 V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
@@ -134,17 +136,18 @@ V = v*(-7*20*tau.^6 + 6*70*tau.^5 -5*84*tau.^4 +4*35*tau.^3);
 velocity(1:ceil(T/(timestep))+offset_2) = V;      
 velocity(1+ceil(T/timestep):end) = 0;
 
-velocity_abrupt = zeros(1,2*ceil(T/(timestep)));
-velocity_abrupt(1:ceil(T/(timestep*2))) = velocity(1:ceil(T/(timestep*2)));
+%velocity_abrupt = zeros(1,2*ceil(T/(timestep)));
+%velocity_abrupt(1:ceil(T/(timestep*2))) = velocity(1:ceil(T/(timestep*2)));
 
- writematrix(position_abrupt,'pos_15_poly.csv')
- writematrix(velocity_abrupt,'vel_15_poly.csv')
+ writematrix(position,'pos_15_poly.csv')
+ writematrix(velocity,'vel_15_poly.csv')
 
-figure(1)
-plot(time(1:2*ceil(T/(timestep))),velocity_abrupt)
+figure
+subplot(2,1,1)
+plot(time,velocity)
 title("velocity profile poly")
 
-figure(2)
-plot(time(1:2*ceil(T/(timestep))),position_abrupt)
+subplot(2,1,2)
+plot(time,position)
 title("position profile poly ")
 
